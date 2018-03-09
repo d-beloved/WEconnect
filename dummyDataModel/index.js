459f536ebbeaa998f4f8a2a11bcc28dc71dc3228
d-beloved <morayodeji@gmail.com>
2018-03-09 01:31:15 +0100

    Completed the sign-up api using dummy data [(Finishes) #155740470]

---
+++ dummyDataModel/index.js
@@ -0,0 +1,13 @@
+import Users from './user';
+// import Business from './BusinessModel';
+// import Category from './CategoryModel';
+// import Review from './ReviewModel';
+
+const models = {
+  Users,
+  // Business,
+  // Category,
+  // Review
+};
+
+export default models;
---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+import router from './routes/router';
+
+// Set up the express app
+const app = express();
+
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Log requests to the console.
+app.use(logger('dev'));
+
+// set router for api endpoints
+router(app);
+app.use('/', router);
+
+
+// Setup a default catch-all route that sends back a welcome message in JSON format.
+app.listen('3001', () => {
+  console.log('WEconnect App is running on port 3000');
+});
+
+// This will be our application entry. Our server is setup here.
