0aa982ce351808b4c3eeb4eee7a47cf92435f4bc
d-beloved <morayodeji@gmail.com>
2018-03-13 08:46:15 +0100

    wrote the test for the index routes

--- tests/routes/index.js
+++ tests/routes/index.js
@@ -1,15 +1,15 @@
 import request from 'supertest';
-import server from '../../server';
+import router from '../../server/routes/router';
 
 describe('/GET: /api/v1/ Tests for index routes', () => {
   it('should return status code 404 when user visit an unregistered route', (done) => {
-    request(server)
+    request(router)
       .get('/api/v1/unregisteredroute')
       .expect(404)
       .end(done);
   });
   it('should return status code 200 when user visits the index route', (done) => {
-    request(server)
+    request(router)
       .get('/api/v1/')
       .expect(200)
       .end(done);
