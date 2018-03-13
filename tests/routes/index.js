6ad0d78a729c0c8f6b42bf59e6508b714121fff7
d-beloved <morayodeji@gmail.com>
2018-03-13 09:06:33 +0100

    Added package-lock.json as part of what to ignore

--- tests/routes/index.js
+++ tests/routes/index.js
@@ -1,15 +1,15 @@
 import request from 'supertest';
-import router from '../../server/routes/router';
+import server from '../../server/server';
 
 describe('/GET: /api/v1/ Tests for index routes', () => {
   it('should return status code 404 when user visit an unregistered route', (done) => {
-    request(router)
+    request(server)
       .get('/api/v1/unregisteredroute')
       .expect(404)
       .end(done);
   });
   it('should return status code 200 when user visits the index route', (done) => {
-    request(router)
+    request(server)
       .get('/api/v1/')
       .expect(200)
       .end(done);
