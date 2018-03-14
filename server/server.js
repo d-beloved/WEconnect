1b7cae3fa2e7d5781c5301c50491cd88f8fc70f4
d-beloved <morayodeji@gmail.com>
2018-03-14 22:06:16 +0100

    Chore - Added the packages for Swagger documentation

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
+import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
+import YAML from 'yamljs';
 import router from './routes/router';
+
+// Set up the Swagger document for API documentation
+const swaggerDocument = YAML.load(`${process.cwd()}/swagger.yaml`);
 
 // Set up the express app
 const server = express();
@@ -16,6 +22,9 @@ server.use(logger('dev'));
 // set router for api endpoints
 server.use('/api/v1', router);
 
+// for serving the Swagger documentation
+server.use(cors({ credentials: true, origin: true }));
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // set the port for the server
 const port = process.env.PORT || 3001;
