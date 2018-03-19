92da5e854dfb26c4e0c64723a4120018a59b414a
d-beloved <morayodeji@gmail.com>
2018-03-19 12:24:31 +0100

    Chore - working on creating the user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,29 @@
-'use strict';
+export default (sequelize. DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        is: { args: /^[A-Za-z_](\w|_|[0-9])*$/, msg: 'Firstname format is invalid! - make sure it is at least 2 characters' },
+      },
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        is: { args: /^[A-Za-z_](\w|_|[0-9])*$/, msg: 'Lastname format is invalid! - make sure it is at least 2 characters' },
+      },
+    },
+  })
+}
 module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define('User', {
     title: DataTypes.STRING
