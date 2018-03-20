fb40383556d24056e99c4cf44feadbbecd85cfdb
d-beloved <morayodeji@gmail.com>
2018-03-20 12:11:22 +0100

    Chore - writing of the models before migration into the database

--- server/models/business.js
+++ server/models/business.js
@@ -1,10 +1,66 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  var Business = sequelize.define('Business', {
-    title: DataTypes.STRING
-  }, {});
-  Business.associate = function(models) {
-    // associations can be defined here
+export default (sequelize, DataTypes) => {
+  const Business = sequelize.define('Business', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    website: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        isUrl: { args: true, msg: 'Web address invalid. Enter a valid web address for your business' },
+      },
+    },
+    phoneno: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    details: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    services: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id'
+      },
+    },
+  });
+
+  Business.associate = (models) => {
+    Business.hasMany(models.Reviews, {
+      foreignKey: 'businessId',
+      as: 'reviews'
+    });
+    Business.belongsTo(models.User, {
+      foreignKey: 'userId',
+      onDelete: 'CASCADE'
+    });
   };
+
   return Business;
 };
--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,4 @@
-export default (sequelize. DataTypes) => {
+export default (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     id: {
       allowNull: false,
@@ -8,28 +8,33 @@ export default (sequelize. DataTypes) => {
     },
     firstName: {
       type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        is: { args: /^[A-Za-z_](\w|_|[0-9])*$/, msg: 'Firstname format is invalid! - make sure it is at least 2 characters' },
-      },
+      allowNull: false
     },
     lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
       validate: {
-        is: { args: /^[A-Za-z_](\w|_|[0-9])*$/, msg: 'Lastname format is invalid! - make sure it is at least 2 characters' },
+        isEmail: { args: true, msg: 'email format is invalid!' },
       },
     },
-  })
-}
-module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
-    title: DataTypes.STRING
-  }, {});
-  User.associate = function(models) {
-    // associations can be defined here
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: { args: [6, 100], msg: 'password is too short! - make sure it is at least 6 characters' },
+      },
+    },
+  });
+
+  // Association is defined here
+  User.associate = (models) => {
+    User.hasMany(models.Business, { foreignKey: 'userId', as: 'businesses' });
+    User.hasMany(models.Reviews, { foreignKey: 'userId', as: 'reviews' });
   };
   return User;
 };
