06a7b07d506af696025cdc538c5b5298e70ed993
d-beloved <morayodeji@gmail.com>
2018-04-01 06:09:29 +0100

    BugFix - refactoring auth.js

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,5 +1,10 @@
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import db from '../models';
 
+const { Business, Reviews } = db;
+
+dotenv.config();
 
 /**
  * @description check if logged in user has a valid session token for protected routes
@@ -11,19 +16,20 @@ import jwt from 'jsonwebtoken';
 export default {
 
   authenticate(req, res, next) {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization.split(' ')[1];
     if (!token) {
       res.status(401).send({ message: 'token is required!' });
     } else {
       // checks if token matches the one provided at login
       jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-        /* eslint-disable no-console */
-        console.log(decoded);
+        console.log(req.body.userId);
         if (err) {
           res.status(401).send({ message: 'Authentication failed!' });
-        } else {
+        } else if (req.body.id === decoded.id) {
           req.user = decoded;
           next();
+        } else {
+          res.status(401).send({ message: 'You don\'t own this resource' });
         }
       });
     }
